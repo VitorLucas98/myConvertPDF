702b2469c259167717da1f8575a6c95431aa3e20
Vitor Lucas <vitorluc.2013@gmail.com>
2025-12-08 10:15:32 -0300

    project created

---
+++ src/main/java/vbotelho/dev/myConvertPdf/MyConvertPdfApplication.java
@@ -0,0 +1,13 @@
+package vbotelho.dev.myConvertPdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyConvertPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyConvertPdfApplication.class, args);
+	}
+
+}
